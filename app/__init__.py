6941044c7c2373f05397db43f948be786e69e653
Emanuel Joao <emanuelhedima@gmail.com>
2025-04-19 19:30:39 +0100

    primeiro yteste

--- app/__init__.py
+++ app/__init__.py
@@ -99,7 +99,7 @@ def create_app():
 
     if os.getenv('FLASK_ENV') == 'production':
         app.config['DEBUG'] = False
-        app.logger.info("Aplicação configurada para modo de produção")
+        app.logger.info("Aplicação, configurada para modo de produção")
     else:
         app.config['DEBUG'] = True
         app.logger.info("Aplicação configurada para modo de desenvolvimento")
