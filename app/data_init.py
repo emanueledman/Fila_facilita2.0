a99e79df4cf760cbea1353ca160e3e41a72da525
Emanuel Joao <emanuelhedima@gmail.com>
2025-05-17 16:43:18 +0100

    Envij

--- app/data_init.py
+++ app/data_init.py
@@ -3485,8 +3485,8 @@ def populate_initial_data(app):
                                 id=str(uuid.uuid4()),
                                 branch_id=branch.id,
                                 weekday=day,
-                                open_time=time(23, 0),
-                                end_time=time(17, 0),
+                                open_time=time(7, 0),
+                                end_time=time(23, 0),
                                 is_closed=False
                             )
                             db.session.add(schedule)
